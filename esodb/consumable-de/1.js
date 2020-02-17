468d29b2f00c8927ac8315b201ce9168f0fd8ef3
ewigejagd <ewigejagd@users.noreply.github.com>
2020-02-17 16:38:58 +0100

    - adding eso consumable tooltip

---
+++ esodb/consumable-de/1.js
@@ -0,0 +1,8 @@
+ElderScrollsBote.Tooltips.registerData({
+    params: {
+        type: "consumable",
+        lang: "de",
+        id: "1"
+    },
+    html: "<div class=\"esb-tooltip\"><p>Einzigartiges Gericht</p><div class=\"esb-tooltip-center\"><h4 class=\"esb-tooltip-setname\">Uhrwerk-Zitronenfilet</h4><em>Clockwork Citrus Filet</em><div class=\"esb-tooltip-seperator\"></div><p>Erhöht <strong>2</strong> Stunden lang Euer maximales Leben um <strong>3326</strong>, Eure Lebensregeneration um <strong>406</strong>, Euer maximale Magicka um <strong>3080</strong> und Eure Magickaregeneration um <strong>338</strong>. (<strong>5 s</strong> Abklingzeit.)</p>Mechanoidenfleisch, perfekt getrocknet und in regionalen Früchten geschmort. Also zumindest in Dingen, die gerade so als Früchte durchgehen.</div></div>"
+});
