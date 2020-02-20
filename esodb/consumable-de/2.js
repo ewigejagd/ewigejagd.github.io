b80596b678e434c3960c338c7c6c7d3df548efa7
ewigejagd <ewigejagd@users.noreply.github.com>
2020-02-20 09:59:35 +0100

    - add new consumable "Verhexte Zuckerschädel"
    - fix typo in consumable "Uhrwerk-Zitronenfilet"

---
+++ esodb/consumable-de/2.js
@@ -0,0 +1,8 @@
+ElderScrollsBote.Tooltips.registerData({
+    params: {
+        type: "consumable",
+        lang: "de",
+        id: "2"
+    },
+    html: "<div class=\"esb-tooltip\"><p>Einzigartiges Gericht</p><div class=\"esb-tooltip-center\"><h4 class=\"esb-tooltip-setname\">Verhexte Zuckerschädel</h4><em>Bewitched Sugar Skulls</em><div class=\"esb-tooltip-seperator\"></div><p>Erhöht <strong>2</strong> Stunden lang Euer maximales Leben um <strong>4620</strong>, Eure maximale Ausdauer und Magicka um <strong>4250</strong> und Eure Lebensregeneration um <strong>462</strong>. Diese Effekte skalieren mit Eurer Stufe. (<strong>6 s</strong> Abklingzeit.)</p>Eine kleine Sammlung bunter Zuckerschädel, gemahlener Mandeln und anderer, merkwürdiger Dinge. Alles ist von einem Hauch Verzauberungen umgeben.</div></div>"
+});
