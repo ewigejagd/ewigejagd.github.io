7c1f022dc3edbe60b158cb22baf761bb458b31cd
ewigejagd <ewigejagd@users.noreply.github.com>
2020-02-20 10:29:33 +0100

    - add new consumable "Knochenbrühe des Rudelführers"

---
+++ esodb/consumable-de/3.js
@@ -0,0 +1,8 @@
+ElderScrollsBote.Tooltips.registerData({
+    params: {
+        type: "consumable",
+        lang: "de",
+        id: "3"
+    },
+    html: "<div class=\"esb-tooltip\"><p>Getränk</p><div class=\"esb-tooltip-center\"><h4 class=\"esb-tooltip-setname\">Knochenbrühe des Rudelführers</h4><em>Pack Leader's Bone Broth</em><div class=\"esb-tooltip-seperator\"></div><p>Erhöht <strong>2</strong> Stunden lang Eure maximale Ausdauer um <strong>4620</strong> und Euer maximales Leben um <strong>5051</strong>. Seid Ihr ein Werwolf, wird das reichhaltige Mark Eure Transformation etwas erleichtern. Diese Effekte skalieren abhängig von Eurer Stufe. (<strong>6 s</strong> Abklingzeit.)</p>Eine dicke, herzhafte Brühe, die aus einheimischen und exotischen Knochen und einer Reihe verschiedener Fleischsorten und Kräutern zubereitet wird. Ziemlich wohlriechend.</div></div>"
+});
